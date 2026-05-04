22af5af29246ee327fa041c8467d20d047c170de
mithlesh <kamaldewangan367@gmail.com>
2026-05-04 15:54:05 +0530

    add icons

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
+import Constants from "expo-constants";
+
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
+const supabaseKey = Constants.expoConfig?.extra?.supabaseKey;
+
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  supabaseUrl,
+  supabaseKey,
   {
     auth: {
       storage: AsyncStorage,
